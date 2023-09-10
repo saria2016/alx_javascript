a8e130bdf920496f642198692d5af58019cbbb24
saria2016 <saria.m.mohammed@gmail.com>
2023-09-10 21:26:16 +0300

    print all characters

--- javascript-network/100-starwars_characters.js
+++ javascript-network/100-starwars_characters.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 // script that prints all characters of a Star Wars movie
 
-const req = require('request');
-const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
-request(url, function (error, response, body) {
+const request = require('request');
+
+request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (error, response, body) {
   if (!error) {
     const characters = JSON.parse(body).characters;
     printCharacters(characters, 0);
