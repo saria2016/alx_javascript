59cf78fe94a984395936f3954426903d47c6e6cf
saria2016 <saria.m.mohammed@gmail.com>
2023-09-10 21:03:34 +0300

    java networks task 0

---
+++ javascript-network/0-statuscode.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// display the status code of a GET request.
+
+const request = require('request');
+
+request(process.argv[2], function (err, response) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('code: ' + response.statusCode);
+  }
+});
