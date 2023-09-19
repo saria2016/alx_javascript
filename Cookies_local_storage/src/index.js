76cc13669161534e50a3ed38a2ea65ec7ea106d4
saria2016 <saria.m.mohammed@gmail.com>
2023-09-19 21:32:14 +0300

    0. Create basic cookie

---
+++ Cookies_local_storage/src/index.js
@@ -0,0 +1,17 @@
+// function setCookies() {
+//     const firstname = document.getElementById('firstname');
+//     const email = document.getElementById('email');
+//     const nameValue = firstname.value;
+//     const emailValue = email.value;
+
+//     document.cookie = `firstname=${nameValue}`;
+//     document.cookie = `email=${emailValue}`;
+
+//     alert("Cookies st Successfully")
+   
+// }
+// function showCookies() {
+//     const cookies = document.cookie;
+//     const cookiesElement = document.getElementById('cookiesValue');
+//     cookiesElement.innerHTML = `Cookies: ${cookies}`;
+// }
